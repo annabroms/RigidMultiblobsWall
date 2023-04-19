modify timestepping with the correct gradient

many_bodyMCMC/langevin.py
@@ -94,7 +94,7 @@ if __name__ == '__main__':
     blob_radius = read.blob_radius
     periodic_length = read.periodic_length
     dt = 4e-2 #time-step: read from file instead
-    dt = 1e-3
+    dt = 1e-2
     weight = 1.0 * read.g
     kT = read.kT
     MCMC = 0
@@ -167,6 +167,8 @@ if __name__ == '__main__':
         #velocity_and_omega = grad + np.sqrt(2.0*kT/dt)*noise
         #print(np.max(grad*dt+np.sqrt(2.0*kT*dt)))
         #print(np.max(grad*dt))
+        gradT = grad[0:6]
+        gradA = grad[6:]
         print(np.max(np.abs(grad[6:])))
 
         #print(gradq)
@@ -184,11 +186,11 @@ if __name__ == '__main__':
             if body.prescribed_kinematics is False:
                 #Here we propose a new location and orientation, by doing langevin dynamics
 
-                trans_vel = -res_trans*grad[i*7:i*7+3]+np.sqrt(res_trans*2.0*kT/dt)*noise[i*6:i*6+3]
+                trans_vel = -res_trans*gradT[i*3:i*3+3]+np.sqrt(res_trans*2.0*kT/dt)*noise[i*6:i*6+3]
                 body.location_new = body.location + dt*trans_vel #the right indices?
                 Q = body.orientation.gradToTourqueMatrix() #is this step problematic?
 
-                rot_vel = - res_rot*Q @ grad[i*7+3:i*7+7]+np.sqrt(res_rot*2.0*kT/dt)*noise[i*6+3:i*6+6]
+                rot_vel = - res_rot*Q @ gradA[i*4:(i+1)*4]+np.sqrt(res_rot*2.0*kT/dt)*noise[i*6+3:i*6+6]
                 quaternion_dt = Quaternion.from_rotation(rot_vel*dt)
                 body.orientation_new = quaternion_dt*body.orientation
                 #print(body.location_new)

multi_bodies/futhark_interaction.py
@@ -1,21 +1,22 @@
 import numpy as np
 import sys
-import importlib
+
 import os.path
 from functools import partial
 
 #sys.path.append('../../..')
 sys.path.append('..')
-#from quaternion_integrator.quaternion import Quaternion
-
+from quaternion_integrator.quaternion import Quaternion
+from rods.tools import pair_histograms as ph
 #import multi_bodies
-import multi_bodies._futhark_interaction as _futhark_interaction
+#import multi_bodies._futhark_interaction as _futhark_interaction
 #from multi_bodies import *
 #import _futhark_interaction as _futhark_interaction
-#import _futhark_interaction as _futhark_interaction
+import _futhark_interaction as _futhark_interaction
 #import futhark_tools._futhark_interaction
 #import futhark_tools as _futhark_interaction
 from futhark_ffi import Futhark
+
 
 context = Futhark(_futhark_interaction)
 
@@ -52,39 +53,41 @@ def calc_body_body_forces_torques_futhark_net(bodies, r_vectors, networkparamete
     force_torque_bodies = context.networkInteraction(networkparameter, locations, orientations)
     return context.fromFuthark(force_torque_bodies)
 
-if __name__ == '__main__':
+#if __name__ == '__main__':
 
     #generate new valid random configuration by checking shortest distance
-    d = 0
-    R = 0.025
-    L = 0.5
+d = 0
+R = 0.025
+L = 0.5
 
-    q1 = Quaternion([1,0,0,0])
-    q2 = Quaternion([1,0,0,0])
+q1 = Quaternion([1,0,0,0])
+q2 = Quaternion([1,0,0,0])
 
-    while d < 2*R:
-        #generate new random configuration
-        x1 = np.random.uniform(-5, 5, 3)
-        x2 = np.random.uniform(-5, 5, 3)
-        q1.random_orientation()
-        q2.random_orientation()
-
-
-        p1,r1 = pair_histograms.endpoints(q1, L, x1)
-        p2,r2 = pair_histograms.endpoints(q2, L, x2)
-
-        d = pair_histograms.distance(p1, r1, p2, r2)
+while d < 2*R:
+    #generate new random configuration
+    x1 = np.random.uniform(-5, 5, 3)
+    x2 = np.random.uniform(-5, 5, 3)
+    q1.random_orientation()
+    q2.random_orientation()
 
 
-    #FT = calc_body_body_force_torque_new(x1,x2,q1,q2)
-    locations = np.zeros(Nbodies, 3)
-    orientations = np.zeros(Nbodies, 4)
-    locations[0] = x1
-    locations[1] = x2
-    orientations[0] = q1
-    orientations[1] = q2
-    epsilon = 20
-    sigma_par = 0.5
-    sigma_ort = 0.5/10
-    FT = context.hgoInteraction(epsilon, sigma_par, sigma_ort, locations, orientations)
-    print(FT[0,:])
+    d = ph.shortestDist(x1,x2,q1,q2,L,R)
+
+
+
+
+#FT = calc_body_body_force_torque_new(x1,x2,q1,q2)
+Nbodies = 2
+locations = np.zeros((Nbodies, 3))
+orientations = np.zeros((Nbodies, 4))
+locations[0] = x1
+locations[1] = x2
+orientations[0] = q1.getAsVector()
+orientations[1] = q2.getAsVector()
+epsilon = 20
+sigma_par = 0.5
+sigma_ort = 0.5/10
+force_torque_bodies = context.hgoInteraction(epsilon, sigma_par, sigma_ort, locations, orientations)
+FT = context.from_futhark(force_torque_bodies)
+print(FT[0])
+    #print(FT[0])

multi_bodies/rods/test_hgo_forcetorque.py
@@ -7,6 +7,15 @@ multi_body_functions.py. See an example in the file examples/boomerang_suspensio
 # from multi_bodies_functions import *
 
 import sys
+sys.path.append('../..')
+
+import multi_bodies._futhark_interaction as _futhark_interaction
+#import futhark_tools._futhark_interaction
+#import futhark_tools as _futhark_interaction
+from futhark_ffi import Futhark
+
+
+context = Futhark(_futhark_interaction)
 
 import matplotlib.pyplot as plt
 
@@ -15,7 +24,7 @@ import matplotlib.pyplot as plt
 
 from tools import pair_histograms as ph
 
-sys.path.append('../..')
+
 import numpy as np
 
 from quaternion_integrator.quaternion import Quaternion
@@ -47,9 +56,10 @@ def calc_body_body_force_torque_new(x1,x2,q1,q2):
 
   # Add forces
   force_torque_bodies[0] = -grad[0:3]
-  force_torque_bodies[2] = -Q1 @ grad[3:7]
+  force_torque_bodies[2] = -grad[3:6]
+
   # Add torques
-  force_torque_bodies[1] = -grad[7:10]
+  force_torque_bodies[1] = -Q1 @ grad[6:10]
   force_torque_bodies[3] = -Q2 @ grad[10:14]
 
   return force_torque_bodies
@@ -66,132 +76,159 @@ if __name__ == '__main__':
 
     q1 = Quaternion([1,0,0,0])
     q2 = Quaternion([1,0,0,0])
+    x1 = np.array([0,0,0])
 
-    while d < 0:
+    while (d < 0) or (d > 2*L):
         #generate new random configuration
         x1 = np.random.uniform(-5, 5, 3)
         x2 = np.random.uniform(-5, 5, 3)
-        q1.random_orientation()
-        q2.random_orientation()
+        #q1.random_orientation()
+        #q2.random_orientation()
 
         d = ph.shortestDist(x1,x2,q1,q2,L,R)
 
+    print("Distance is %.2e" % d)
+    FT_python = calc_body_body_force_torque_new(x1,x2,q1,q2)
+    print("Anna HGO")
+    print(FT_python)
 
-    FT = calc_body_body_force_torque_new(x1,x2,q1,q2)
-    print(FT[0,:])
+    Nbodies = 2
+    locations = np.zeros((Nbodies, 3))
+    orientations = np.zeros((Nbodies, 4))
+    locations[0] = x1
+    locations[1] = x2
+    orientations[0] = q1.getAsVector()
+    orientations[1] = q2.getAsVector()
 
+    #NB - these parameters must be the same as in the potential definition
+    epsilon = 10
+    sigma_par = 1
+    sigma_ort = 1/10
+    print(sigma_ort)
+    force_torque_bodies = context.hgoInteraction(epsilon, sigma_par, sigma_ort, locations, orientations)
+    FT = context.from_futhark(force_torque_bodies)
+    print("Futhark HGO")
+    print(FT)
+    print("Difference in each component")
+    print(FT_python-FT)
 
-    case = 3
-
-    # Next, we want to look at time-scales
-    if case == 1:
-        delta = 0.01
-        q1 = Quaternion([1,0,0,0])
-        q2 = Quaternion([np.sqrt(2)/2, 0, np.sqrt(2)/2, 0])
-
-        x1 = np.array([0,0,0])
-        x2 = np.array([0,0,L/2+R+delta])
-    elif case == 2:
-        delta = 0.1
-        q1 = Quaternion([1,0,0,0])
-        # Direction of the second particle is [1 1 0]
-        q2 = Quaternion([np.sqrt(2)/2, -1/2, 1/2, 0])
-        x1 = np.array([0,0,0])
-        x2 = np.array([2*R+delta,0,0])
-    else:
-        delta = 0.001
-        q1 = Quaternion([1,0,0,0])
-        q2 = Quaternion([1,0,0,0])
-
-        x1 = np.array([0,0,0])
-        x2 = np.array([2*R+delta,0,0])
+    print("Difference in torque on second particle, but for sign")
+    print(FT[3]+FT_python[3])
 
 
+    ode = 0
+    if ode:
+        case = 3
+
+        # Next, we want to look at time-scales
+        if case == 1:
+            delta = 0.01
+            q1 = Quaternion([1,0,0,0])
+            q2 = Quaternion([np.sqrt(2)/2, 0, np.sqrt(2)/2, 0])
+
+            x1 = np.array([0,0,0])
+            x2 = np.array([0,0,L/2+R+delta])
+        elif case == 2:
+            delta = 0.1
+            q1 = Quaternion([1,0,0,0])
+            # Direction of the second particle is [1 1 0]
+            q2 = Quaternion([np.sqrt(2)/2, -1/2, 1/2, 0])
+            x1 = np.array([0,0,0])
+            x2 = np.array([2*R+delta,0,0])
+        else:
+            delta = 0.001
+            q1 = Quaternion([1,0,0,0])
+            q2 = Quaternion([1,0,0,0])
+
+            x1 = np.array([0,0,0])
+            x2 = np.array([2*R+delta,0,0])
 
 
-    #Check the start distance
-    p1,r1 = ph.endpoints(q1, L, x1)
-    p2,r2 = ph.endpoints(q2, L, x2)
-    #d = ph.distance(p1, r1, p2, r2)
-    cd = ph.centerDist(x1,x2)
-    sd = ph.shortestDist(x1,x2,q1,q2,L,R)
-    # print(cd)
-    # print(sd)
 
 
-    #time step the dynamics
-    dt = 1e-2 #time step size #Was 1e-3 here before
-    N = 100 # number of time steps
-    alphaVec = np.zeros(N)
-    thetaVec = np.zeros(N)
-    phiVec = np.zeros(N)
-    ccVec = np.zeros(N)
-    sVec = np.zeros(N)
-    potVec = np.zeros(N)
-    tVec = np.zeros(N)
-
-    for i in range(N):
-        vel = calc_body_body_force_torque_new(x1,x2,q1,q2)
-        x1 = x1 + dt*vel[0]
-        x2 = x2 + dt*vel[2]
-        print(vel)
-        quat1_dt = Quaternion.from_rotation(vel[1]*dt)
-        q1 = quat1_dt*q1
-        quat2_dt = Quaternion.from_rotation(vel[3]*dt)
-
-        q2 = quat2_dt*q2
-
-        #compute angles and distances between the green_particles
-        alphaDist = ph.getAlpha(x1,x2)
-        r,theta,phi = ph.spherical_q2(q1,q2,x2,L)
-        ccDist = ph.centerDist(x1,x2)
-        potVal = ph.getPotential(x1,x2,q1.getAsVector(),q2.getAsVector())
-        sDist = ph.shortestDist(x1,x2,q1,q2,L,R)
-
-        alphaVec[i] = alphaDist
-        thetaVec[i] = theta
-        phiVec[i] = phi
-        ccVec[i] = ccDist
-        sVec[i] = sDist
-        potVec[i] = potVal
-        tVec[i] = i*dt
+        #Check the start distance
+        p1,r1 = ph.endpoints(q1, L, x1)
+        p2,r2 = ph.endpoints(q2, L, x2)
+        #d = ph.distance(p1, r1, p2, r2)
+        cd = ph.centerDist(x1,x2)
+        sd = ph.shortestDist(x1,x2,q1,q2,L,R)
+        # print(cd)
+        # print(sd)
 
 
-    fig,ax = plt.subplots()
-    plt.plot(tVec,alphaVec)
-    ax.set_ylabel('alpha')
-    ax.set_xlabel('t')
-    fig.savefig('alpha_scale.png')
+        #time step the dynamics
+        dt = 1e-2 #time step size #Was 1e-3 here before
+        N = 100 # number of time steps
+        alphaVec = np.zeros(N)
+        thetaVec = np.zeros(N)
+        phiVec = np.zeros(N)
+        ccVec = np.zeros(N)
+        sVec = np.zeros(N)
+        potVec = np.zeros(N)
+        tVec = np.zeros(N)
 
-    fig,ax = plt.subplots()
-    plt.plot(tVec,thetaVec)
-    ax.set_ylabel('theta')
-    ax.set_xlabel('t')
-    fig.savefig('theta_scale.png')
+        for i in range(N):
+            vel = calc_body_body_force_torque_new(x1,x2,q1,q2)
+            x1 = x1 + dt*vel[0]
+            x2 = x2 + dt*vel[2]
+            print(vel)
+            quat1_dt = Quaternion.from_rotation(vel[1]*dt)
+            q1 = quat1_dt*q1
+            quat2_dt = Quaternion.from_rotation(vel[3]*dt)
 
-    fig,ax = plt.subplots()
-    plt.plot(tVec,phiVec)
-    ax.set_ylabel('phi')
-    ax.set_xlabel('t')
-    fig.savefig('phi_scale.png')
+            q2 = quat2_dt*q2
 
-    fig,ax = plt.subplots()
-    plt.plot(tVec,ccVec)
-    ax.set_ylabel('center dist')
-    ax.set_xlabel('t')
-    fig.savefig('center_dist_scale.png')
+            #compute angles and distances between the green_particles
+            alphaDist = ph.getAlpha(x1,x2)
+            r,theta,phi = ph.spherical_q2(q1,q2,x2,L)
+            ccDist = ph.centerDist(x1,x2)
+            potVal = ph.getPotential(x1,x2,q1.getAsVector(),q2.getAsVector())
+            sDist = ph.shortestDist(x1,x2,q1,q2,L,R)
 
-    fig,ax = plt.subplots()
-    plt.plot(tVec,sVec)
-    ax.set_ylabel('shortes dist')
-    ax.set_xlabel('t')
-    fig.savefig('shortest_dist_scale.png')
+            alphaVec[i] = alphaDist
+            thetaVec[i] = theta
+            phiVec[i] = phi
+            ccVec[i] = ccDist
+            sVec[i] = sDist
+            potVec[i] = potVal
+            tVec[i] = i*dt
 
-    fig,ax = plt.subplots()
-    plt.plot(tVec,potVec)
-    ax.set_ylabel('potential')
-    ax.set_xlabel('t')
-    fig.savefig('potential_scale.png')
+
+        fig,ax = plt.subplots()
+        plt.plot(tVec,alphaVec)
+        ax.set_ylabel('alpha')
+        ax.set_xlabel('t')
+        fig.savefig('alpha_scale.png')
+
+        fig,ax = plt.subplots()
+        plt.plot(tVec,thetaVec)
+        ax.set_ylabel('theta')
+        ax.set_xlabel('t')
+        fig.savefig('theta_scale.png')
+
+        fig,ax = plt.subplots()
+        plt.plot(tVec,phiVec)
+        ax.set_ylabel('phi')
+        ax.set_xlabel('t')
+        fig.savefig('phi_scale.png')
+
+        fig,ax = plt.subplots()
+        plt.plot(tVec,ccVec)
+        ax.set_ylabel('center dist')
+        ax.set_xlabel('t')
+        fig.savefig('center_dist_scale.png')
+
+        fig,ax = plt.subplots()
+        plt.plot(tVec,sVec)
+        ax.set_ylabel('shortes dist')
+        ax.set_xlabel('t')
+        fig.savefig('shortest_dist_scale.png')
+
+        fig,ax = plt.subplots()
+        plt.plot(tVec,potVec)
+        ax.set_ylabel('potential')
+        ax.set_xlabel('t')
+        fig.savefig('potential_scale.png')
 
     ########################################################
     # #Now, we want to compute the Lyaponov exponent